39e5de34ffa60fa517267ac81868ba9edd44f808
cmiles3 <cmiles3@gmail.com>
2022-07-05 23:37:58 -0500

    Drafted String iteration practice code - BROKEN

---
+++ code/re-learn_String iteration.js
@@ -0,0 +1,19 @@
+//BROKEN CODE
+/*
+
+// ** Function to iterate a string - re-practice
+// ** Spell out the chosen heroes name, one letter at a time
+
+var x = 0
+overW = 'overwatch'
+hero = ['mei', 'reaper', 'widow', 'junkrat'];
+heroName = hero[x]
+
+for (x = 0; x < heroName.length - 1; console.log(x)) {
+  x++;
+    if (x == heroName.length - 1) {
+        console.log('No More Heroes');
+    }
+}
+
+*/
