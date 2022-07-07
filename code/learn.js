329cb460faaba967190ef40f964e3aeb8bdb0204
cmiles3 <cmiles3@gmail.com>
2022-07-06 23:22:18 -0500

    practicing the basics

--- code/learn.js
+++ code/learn.js
@@ -1,3 +1,25 @@
-// ready to learn
+// loop iteration practice
 
+// Conditional
+x = 0
+function testNum(a) {
+    let result;
+    if (a > 0) {
+      result = 'positive';
+    } else {
+      result = 'NOT positive';
+    }
+    return result;
+  }
+  console.log(testNum(-5));
+  // expected output: "NOT positive"
 
+// Loop
+  let str = '';
+
+  for (let i = 0; i < 9; i++) {
+    str = str + i;
+  }
+
+  console.log(str);
+  // expected output: "012345678"
