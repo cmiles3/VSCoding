cb4ca6feaa54da675722b14f6e9e60402e78bd8d
cmiles3 <cmiles3@gmail.com>
2022-07-08 10:34:53 -0500

    Basics practice - Loops & iterations

---
+++ code/learn_Basics-Loops & Iterations.js
@@ -0,0 +1,11 @@
+// Loops & iterations
+
+// FOR LOOPS
+for (let step = 0; step < 5; step++) {
+    // Runs 5 times, with values of step 0 through 4.
+    console.log('Walking east one step');
+}
+/*  For Loop example:
+for ([initialExpression]; [conditionExpression]; [incrementExpression])
+  statement
+*/
