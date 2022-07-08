47e50e0837aa036cd3d0fa02c15afac007dbffaf
cmiles3 <cmiles3@gmail.com>
2022-07-07 23:20:29 -0500

    Basics Practice - Strings & Arrays

---
+++ code/learn_StringArrayBasics.js
@@ -0,0 +1,16 @@
+// BASICS PRACTICE -- Strings and Arrays
+// Selecting indices in a string array
+// Also, returning a boolean conditional
+//
+
+let hero
+hero = 'Soldier76'
+console.log(hero);
+console.log(hero[0]);
+console.log(hero[3] == 'd');
+if (hero[4] == 'i') {
+    console.log('Good work Soldier!');
+} else {
+    (console.log('Not a young man anymore'));
+}
+console.log(hero[hero.length - 1]);
