3347c53e4aae627629227af4bea299b410cb3cc4
cmiles3 <cmiles3@gmail.com>
2022-07-11 23:30:40 -0500

    Adding loop to array practice

--- code/learn_ArrayPractice.js
+++ code/learn_ArrayPractice.js
@@ -3,4 +3,13 @@
 
 let heroPool = ['Lucy', 'Hannah', 'Curtis']
 
-console.log('The main hero is:  ' + heroPool[0]);
+// console.log('The main hero is:  ' + heroPool[0]);
+
+// adding a loop to the practice code
+let x = heroPool.length
+let y = 0
+console.log(x);
+while (y < x) {
+    console.log(heroPool[y]);
+    y++;
+}
