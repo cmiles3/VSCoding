65f19ef4808c8e521c1470e04172d2fa0305b542
cmiles3 <cmiles3@gmail.com>
2022-07-23 19:12:02 -0500

    Lotto numPick code practice

---
+++ code/learn_NumSwap_lotto.js
@@ -0,0 +1,29 @@
+// BROKEN CODE
+// Intended attempt at old lotto code from UT CS class.
+//
+
+/*
+
+// console.log(arr);
+let num = 0;
+let arr;
+arr = [];
+lotArr = [];
+
+function lotPick (p1, p2, p3) {
+    lotArr.push(p1);
+    lotArr.push(p2);
+    lotArr.push(p3);
+
+    while (num <= 2) {
+    //    console.log(num)
+        arr.push(num);
+        console.log("Num is:  " + num);
+        num++;
+    }
+}
+console.log(lotPick(1, 5, 3));
+
+//console.log(lotArr);
+
+*/
