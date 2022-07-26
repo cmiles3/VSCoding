d2add2d4cca6ecc71df065cc7181497c7c06ad81
cmiles3 <cmiles3@gmail.com>
2022-07-25 23:39:28 -0500

    mdn_write and learning

--- code/mdn_grammar_and_types.js
+++ code/mdn_grammar_and_types.js
@@ -2,7 +2,7 @@
 //  Grammar and Types
 //
 //  Declarations
-var x; var y = 0;
+var xBar; var yBar = 0;
 let canA; let canB = 0;
 const cupA = 1; const cupB = 0;
 
@@ -10,11 +10,11 @@ const cupA = 1; const cupB = 0;
 let { bar } = foo;
 
 //  Evaluating Variables
-var a;
-console.log('The value of a is ' + a); // The value of a is undefined
+var aBat;
+console.log('The value of a is ' + aBat); // The value of a is undefined
 
-console.log('The value of b is ' + b); // The value of b is undefined
-var b;
+console.log('The value of b is ' + bBat); // The value of b is undefined
+var bBat;
 // This one may puzzle you until you read 'Variable hoisting' below
 
 console.log('The value of c is ' + c); // Uncaught ReferenceError: c is not defined
@@ -24,3 +24,29 @@ console.log('The value of x is ' + x); // The value of x is undefined
 
 console.log('The value of y is ' + y); // Uncaught ReferenceError: y is not defined
 let y;
+
+/* You can use undefined to determine whether a variable has a value.
+In the following code, the variable input is not assigned a value, and the if statement evaluates to true. */
+
+let input;
+if (input === undefined) {
+    doThis();
+} else  {
+    doThat();
+}
+
+/* The undefined value behaves as false when used in a boolean context.
+For example, the following code executes the function myFunction because the myArray element is undefined: */
+
+const myArray = [];
+if (!myArray[0]) myFunction();
+
+// The undefined value converts to NaN when used in numeric context.
+
+let a;
+a + 2; // Evaluates to NaN
+
+// When you evaluate a null variable, the null value behaves as 0 in numeric contexts and as false in boolean contexts. For example:
+
+const n = null;
+console.log(n * 32); // Will log 0 to the console
