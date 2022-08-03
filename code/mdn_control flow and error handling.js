1a6ec0bf63b1c9df0dd09a85fa24e82dc9868e53
cmiles3 <cmiles3@gmail.com>
2022-08-02 22:22:25 -0500

    mdn_control flow notes updated

--- code/mdn_control flow and error handling.js
+++ code/mdn_control flow and error handling.js
@@ -2,9 +2,41 @@
 //
 // I don't understand how this Left Shift assignment works
 //
+/*
+
 let a = 5;
 let b = 5;
 a <<= 1;
 b **= 1
 console.log(a);
 console.log(b);
+
+
+
+// BLOCK STATEMENTS
+let x = 0
+while (x < 10) {
+    console.log(x);
+    x++;
+}
+// In this ^^ example, { x++ } is the block statement
+
+// CONDITIONAL STATEMENTS
+//
+// IF...ELSE Statement
+//
+if (5 < 10) {
+    console.log("That's cool");
+}   else {
+    console.log("Not cool bro");
+}
+
+if (2 == 8) {
+    console.log("That was easy")
+}   else if (2 > 8) {
+    console.log("That was big")
+}   else {
+    console.log("We are done here")
+}
+*/
+// Left off at Falsy values
